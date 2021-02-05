36795811b2566a1dcc33a0d9f1836f622a28e481
willis18 <willis18@naver.com>
2021-02-05 19:45:17 +0900

    Initial Commit

---
+++ hello10-3Tests/hello10_3Tests.swift
@@ -0,0 +1,33 @@
+//
+//  hello10_3Tests.swift
+//  hello10-3Tests
+//
+//  Created by 김송현 on 2021/02/05.
+//
+
+import XCTest
+@testable import hello10_3
+
+class hello10_3Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
